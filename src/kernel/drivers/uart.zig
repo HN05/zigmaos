c5dcfebda35add3d2c0151710e0046b6640c0f31
HN05 <henrik.nordvik@gmail.com>
2026-07-05 03:30:46 +0200

    refactored uart

--- src/kernel/drivers/uart.zig
+++ src/kernel/drivers/uart.zig
@@ -11,80 +11,23 @@ const conc = kernel.concurrency;
 /// some have different meanings for
 /// read vs write.
 /// see http://byterunner.com/16550.html
-const receive_holding_register = 0; // receive holding register (for input bytes)
-const transmit_holding_register = 0; // transmit holding register (for output bytes)
-const interrupt_enable_register = 1; // interrupt enable register
-const interrupt_enable_receive = 1 << 0;
-const interrupt_enable_transmit = 1 << 1;
-const fifo_control_register = 2; // FIFO control register
-const fifo_enable = 1 << 0;
-const fifo_clear = 3 << 1; // clear the content of the two FIFOs
-const interrupt_status_register = 2; // interrupt status register
-const line_control_register = 3; // line control register
-const line_control_eight_bits = 3 << 0;
-const line_control_baud_latch = 1 << 7; // special mode to set baud rate
-const line_status_register = 5; // line status register
-const line_status_receive_ready = 1 << 0; // input is waiting to be read from RHR
-const line_status_transmit_idle = 1 << 5; // THR can accept another character to send
 
-const Transmit = struct {
-    const buffer_size = 32;
-
-    lock: conc.Mutex = .init(.spin, "uart transmit lock"),
-    buffer: [buffer_size]u8 = [_]u8{0} ** buffer_size,
-    write_count: u64 = 0, // write next to uart_tx_buf[uart_tx_w % UART_TX_BUF_SIZE]
-    read_count: u64 = 0, // read next from uart_tx_buf[uart_tx_r % UART_TX_BUF_SIZE]
-
-    pub fn isFull(self: *Transmit) bool {
-        return self.write_count == (self.read_count + buffer_size);
-    }
-
-    pub fn isEmpty(self: *Transmit) bool {
-        return self.write_count == self.read_count;
-    }
-
-    pub fn sleep(self: *Transmit) void {
-        self.lock.sleepOn(&self.read_count);
-    }
-
-    pub fn transmitChar(self: *Transmit, char: u8) void {
-        self.buffer[self.write_count % buffer_size] = char;
-        self.write_count += 1;
-    }
-
-    pub fn readChar(self: *Transmit) u8 {
-        const character = self.buffer[self.read_count % buffer_size];
-        self.read_count += 1;
-
-        // maybe uartputc() is waiting for space in the buffer.
-        execution.scheduler.wakeup(&self.read_count);
-        return character;
-    }
-};
-var transmit = Transmit{};
+// set baud rate to 38.4K
+const baud_rate = 0x0003;
 
 pub fn init() void {
-    // disable interrupts.
-    writeReg(interrupt_enable_register, 0x00);
+    interrupt_enable_register.disableInterrupts();
 
-    // special mode to set baud rate.
-    writeReg(line_control_register, line_control_baud_latch);
+    line_control_register.startEditBaudRate();
+    line_control_register.setBaudRate(baud_rate);
 
-    // LSB for baud rate of 38.4K.
-    writeReg(0, 0x03);
-
-    // MSB for baud rate of 38.4K.
-    writeReg(1, 0x00);
-
-    // leave set-baud mode,
-    // and set word length to 8 bits, no parity.
-    writeReg(line_control_register, line_control_eight_bits);
+    // set word length to 8 bits, no parity.
+    line_control_register.endEditBaudRate(.eight_bits_no_parity);
 
     // reset and enable FIFOs.
-    writeReg(fifo_control_register, fifo_enable | fifo_clear);
+    fifo_control_register.resetAndEnable();
 
-    // enable transmit and receive interrupts.
-    writeReg(interrupt_enable_register, interrupt_enable_receive | interrupt_enable_transmit);
+    interrupt_enable_register.enableInterrupts();
 }
 
 // add a character to the output buffer and tell the
@@ -119,8 +62,8 @@ pub fn putCharSync(ch: u8) void {
     if (log.panicked.*) while (true) {};
 
     // wait for Transmit Holding Empty to be set in LSR.
-    while ((readReg(line_status_register) & line_status_transmit_idle) == 0) {}
-    writeReg(transmit_holding_register, ch);
+    while (!line_status_register.isTransmitReady()) {}
+    holding_register.write(ch);
 }
 
 /// if the UART is idle, and a character is waiting
@@ -129,33 +72,23 @@ pub fn putCharSync(ch: u8) void {
 /// called from both the top- and bottom-half.
 pub fn start() void {
     while (true) {
-        if (transmit.isEmpty()) {
-            // transmit buffer is empty.
-            return;
-        }
+        // transmit buffer is empty.
+        if (transmit.isEmpty()) return;
 
-        if ((readReg(line_status_register) & line_status_transmit_idle) == 0) {
-            // the UART transmit holding register is full,
-            // so we cannot give it another byte.
-            // it will interrupt when it's ready for a new byte.
-            return;
-        }
+        // the UART transmit holding register is full,
+        // so we cannot give it another byte.
+        // it will interrupt when it's ready for a new byte.
+        if (!line_status_register.isTransmitReady()) return;
 
         const character = transmit.readChar();
-
-        writeReg(transmit_holding_register, character);
+        holding_register.write(character);
     }
 }
 
 /// read one input character from the UART.
 /// return NotReady if none is waiting.
 pub fn getCharacter() !u8 {
-    if ((readReg(line_status_register) & line_status_receive_ready) != 0) {
-        // input data is ready.
-        return readReg(receive_holding_register);
-    } else {
-        return error.NotReady;
-    }
+    return if (line_status_register.isReceiveReady()) holding_register.read() else error.NotReady;
 }
 
 /// handle a uart interrupt, raised because input has
@@ -175,14 +108,133 @@ pub fn interrupt() void {
     start();
 }
 
-fn getRegPtr(register_offset: usize) *volatile u8 {
-    return memlayout.uart0_base_address.add(register_offset).asPtr(*volatile u8);
-}
+const transmit = struct {
+    const buffer_size = 32;
 
-fn readReg(register_offset: usize) u8 {
-    return getRegPtr(register_offset).*;
-}
+    var lock: conc.Mutex = .init(.spin, "uart transmit lock");
+    var buffer: [buffer_size]u8 = [_]u8{0} ** buffer_size;
+    var write_count: u64 = 0;
+    var read_count: u64 = 0;
 
-fn writeReg(register_offset: usize, value: u8) void {
-    getRegPtr(register_offset).* = value;
-}
+    pub fn isFull() bool {
+        return write_count == (read_count + buffer_size);
+    }
+
+    pub fn isEmpty() bool {
+        return write_count == read_count;
+    }
+
+    pub fn sleep() void {
+        lock.sleepOn(&read_count);
+    }
+
+    pub fn transmitChar(char: u8) void {
+        buffer[write_count % buffer_size] = char;
+        write_count += 1;
+    }
+
+    pub fn readChar() u8 {
+        const character = buffer[read_count % buffer_size];
+        read_count += 1;
+
+        // maybe uartputc() is waiting for space in the buffer.
+        execution.scheduler.wakeup(&read_count);
+        return character;
+    }
+};
+
+// for transfering bytes
+// read goes to receive
+// write goes to transmit
+const holding_register = Register{ .offset = 0, .kind = .readwrite };
+
+const interrupt_enable_register = struct {
+    const register = Register{ .offset = 1, .kind = .write };
+    const enable_receive = 1 << 0;
+    const enable_transmit = 1 << 1;
+
+    pub fn disableInterrupts() void {
+        register.write(0);
+    }
+
+    // enable transmit and receive interrupts.
+    pub fn enableInterrupts() void {
+        register.write(enable_receive | enable_transmit);
+    }
+};
+
+const fifo_control_register = struct {
+    const enable_flag = 1 << 0;
+    const clear_flag = 3 << 1; // clear the content of the two FIFOs
+    const register = Register{ .offset = 2, .kind = .write };
+
+    pub fn resetAndEnable() void {
+        register.write(enable_flag | clear_flag);
+    }
+};
+
+const line_control_register = struct {
+    const BitMode = enum(u8) {
+        eight_bits_no_parity = 3 << 0,
+    };
+    const baud_latch = 1 << 7; // special mode to set baud rate
+
+    const register = Register{ .offset = 3, .kind = .write };
+    const lsb_register = Register{ .offset = 0, .kind = .write };
+    const msb_register = Register{ .offset = 1, .kind = .write };
+
+    pub fn startEditBaudRate() void {
+        // start to negotiate baud rate
+        register.write(baud_latch);
+    }
+
+    pub fn setBaudRate(value: u16) void {
+        const lsb: u8 = @truncate(value);
+        const msb: u8 = @truncate(value >> 8);
+        lsb_register.write(lsb);
+        msb_register.write(msb);
+    }
+
+    pub fn endEditBaudRate(bit_mode: BitMode) void {
+        register.write(@intFromEnum(bit_mode));
+    }
+};
+
+const line_status_register = struct {
+    const line_status_receive_ready = 1 << 0; // input is waiting to be read from RHR
+    const line_status_transmit_idle = 1 << 5; // THR can accept another character to send
+    const register = Register{ .offset = 5, .kind = .read };
+
+    pub fn isReceiveReady() bool {
+        return (register.read() & line_status_receive_ready) != 0;
+    }
+
+    pub fn isTransmitReady() bool {
+        return (register.read() & line_status_transmit_idle) != 0;
+    }
+};
+
+const Register = struct {
+    const RegisterKind = enum {
+        read,
+        write,
+        readwrite,
+    };
+    offset: usize,
+    kind: RegisterKind,
+
+    fn getPtr(self: Register) *volatile u8 {
+        return memlayout.uart0_base_address.add(self.offset).asPtr(*volatile u8);
+    }
+
+    pub fn read(self: Register) u8 {
+        if (self.kind == .write) @panic("can't read for write only register uart");
+        return self.getPtr().*;
+    }
+
+    pub fn write(self: Register, value: u8) void {
+        if (self.kind == .read) @panic("can't write for read only register uart");
+        self.getPtr().* = value;
+    }
+};
+
